1e82c2e0532092028ce9d73682c46574f7269976
Richard Jimenez <rjimenezvelasco@gmail.com>
2025-05-19 19:35:19 -0400

    version 0002

---
+++ src/components/Header.tsx
@@ -0,0 +1,6 @@
+function Header() {
+  const nombre = "Cabecera";
+  return <h3> {nombre}</h3>;
+}
+
+export default Header;
---
+++ src/components/Titulo.tsx
@@ -0,0 +1,11 @@
+function Titulo() {
+  return (
+    <div className="App">
+      <h1>Bienvenido a React hola mundo</h1>
+      <p>
+        Desde Titulo <code>src/App.tsx</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+export default Titulo;
